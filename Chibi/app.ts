b96dac540c2cfcd3a4b3d82df14adeb02d1e8a6e
Bas Pennings <baspster@gmail.com>
2014-09-10 23:00:27 +0200

    Initial commit

---
+++ Chibi/app.ts
@@ -0,0 +1,35 @@
+﻿$(function () {
+    var BACKSPACE = 8;
+    var ENTER = 13;
+
+    var prompt = '<span>&gt;</span> ';
+    var $console = $('#console');
+    var $input = $('#input');
+
+    $input.append(prompt);
+    $input.keydown(function (event) {
+        if (event.which === BACKSPACE &&
+            this.innerHTML.length === prompt.length) {
+            event.preventDefault();
+        }
+
+        if (event.which === ENTER) {
+            var cmd = this.innerHTML.substr(prompt.length)
+            var echo = '<div class="echo"><span>&gt;</span> ' + cmd + '</div>';
+            var output, result;
+
+            $(echo).insertBefore($input);
+            try {
+                result = eval(cmd);
+                output = '<div class="result"><span>&lt;</span> ' + result + '</div>';
+            }
+            catch (error) {
+                output = '<div class="error">' + error + '</div>';
+            }
+
+            $(output).insertBefore($input);
+            this.innerHTML = prompt;
+            event.preventDefault();
+        }
+    });
+});
---
+++ Chibi/chibi.js
@@ -0,0 +1,1 @@
+﻿//# sourceMappingURL=chibi.js.map

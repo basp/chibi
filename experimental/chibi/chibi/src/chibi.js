58a03715e4ef2297e3e735a96219cca6e84d174f
Bas Pennings <baspster@gmail.com>
2015-01-21 19:32:51 +0100

    Just a bunch of experimental stuff

---
+++ experimental/chibi/chibi/src/chibi.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="../Scripts/typings/jquery/jquery.d.ts" />
+
+var MemoryCommandStore = (function () {
+    function MemoryCommandStore() {
+        this.commands = [];
+        this.clientIndex = -1;
+    }
+    MemoryCommandStore.prototype.remember = function (command) {
+        if (command !== this.lastCommand()) {
+            this.commands.push(command);
+        }
+
+        this.clientIndex = this.commands.length;
+    };
+
+    MemoryCommandStore.prototype.prev = function () {
+        if (this.clientIndex > 0) {
+            this.clientIndex -= 1;
+            var cmd = this.commands[this.clientIndex];
+            return cmd;
+        }
+        1;
+
+        return undefined;
+    };
+
+    MemoryCommandStore.prototype.next = function () {
+        if (this.clientIndex < this.commands.length) {
+            this.clientIndex += 1;
+            var cmd = this.commands[this.clientIndex];
+            return cmd;
+        }
+
+        return undefined;
+    };
+
+    MemoryCommandStore.prototype.lastCommand = function () {
+        if (this.commands.length > 0) {
+            return this.commands[this.commands.length - 1];
+        }
+
+        return undefined;
+    };
+    return MemoryCommandStore;
+})();
+
+window['chibi'] = new MemoryCommandStore();
+//# sourceMappingURL=chibi.js.map
---
+++ experimental/chibi/chibi/src/chibi.ts
@@ -0,0 +1,50 @@
+﻿/// <reference path="../Scripts/typings/jquery/jquery.d.ts" />
+
+interface CommandStore {
+    remember(cmd: string): void
+    prev(): string;
+    next(): string;
+}
+
+class MemoryCommandStore implements CommandStore {
+    private commands: string[] = []
+    private clientIndex: number = -1;
+
+    remember(command: string) {
+        if (command !== this.lastCommand()) {
+            this.commands.push(command);
+        }
+
+        this.clientIndex = this.commands.length;
+    }
+
+    prev() {
+        if (this.clientIndex > 0) {
+            this.clientIndex -= 1;
+            var cmd = this.commands[this.clientIndex];
+            return cmd;
+        }1
+
+        return undefined;
+    }
+
+    next() {
+        if (this.clientIndex < this.commands.length) {
+            this.clientIndex += 1;
+            var cmd = this.commands[this.clientIndex];
+            return cmd;
+        }
+
+        return undefined;
+    }
+
+    private lastCommand() {
+        if (this.commands.length > 0) {
+            return this.commands[this.commands.length - 1];
+        }
+
+        return undefined;
+    }
+}
+
+window['chibi'] = new MemoryCommandStore();
